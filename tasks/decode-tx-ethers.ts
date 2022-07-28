2ab79cf4b746f45eac88546e3fba45a8a3b5d727
Peter Graham <peterghm@gmail.com>
2022-07-28 09:54:40 -0400

    Update decode-tx-ethers.ts

--- tasks/decode-tx-ethers.ts
+++ tasks/decode-tx-ethers.ts
@@ -1,10 +1,10 @@
 import { ethers } from 'ethers';
-import { abis } from '../data/abi';
-import poolTransactions from '../data/tx/ethereum.0x0Ce6a5fF5217e38315f87032CF90686C96627CAA.json';
+import { abis } from '../data/abis';
+import { txs } from '../data/txs';
 
 const main = async () => {
   const contractAddress = '0x0Ce6a5fF5217e38315f87032CF90686C96627CAA';
-  const tx = poolTransactions[0];
+  const tx = txs.ethereum.main[contractAddress][0];
 
   const poolInterface = new ethers.utils.Interface(
     JSON.stringify(abis.ethereum.main[contractAddress]),
