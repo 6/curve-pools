307fdd6c96b7bffee7bf01ecba26cb0f20a2a21e
Peter Graham <peterghm@gmail.com>
2022-07-28 18:29:56 -0400

    fix types

--- tasks/process-pool-transactions.ts
+++ tasks/process-pool-transactions.ts
@@ -2,14 +2,15 @@ import { writeJSON } from '../src/utils/write-json';
 import { ethers } from 'ethers';
 import { getTxs } from '../data/txs';
 import { getABIInterface } from '../data/abis';
+import { getPools } from '../data/pools';
 
 const main = async () => {
   // For now, just focus on limited set of data:
   const network = 'ethereum';
   const poolType = 'main';
 
-  const poolData = pools[network][poolType];
-  for (const pool of poolData) {
+  const pools = await getPools({ network, poolType });
+  for (const pool of pools) {
     const contractAddress = pool.address;
     const txs = await getTxs({ network, poolType, contractAddress });
     const poolInterface = await getABIInterface({ network, poolType, contractAddress });
