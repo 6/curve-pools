390dd53f863655b62f8809b28ffe537f806fea7d
Peter Graham <peterghm@gmail.com>
2022-07-27 23:58:18 -0400

    update all pool abis

--- scripts/update-pool-abis.ts
+++ scripts/update-pool-abis.ts
@@ -1,14 +1,16 @@
 import { sleep } from '../src/utils/sleep';
 import { explorers } from '../src/utils/etherscan';
 import { writeJSON } from '../src/utils/write-json';
+import ethereumMainPools from '../data/pools/ethereum.main.json';
 
 const main = async () => {
-  const contractAddress = '0xbEbc44782C7dB0a1A60Cb6fe97d0b483032FF1C7';
-
-  await sleep(500);
-  const abi = await explorers.etherscan.mainnet.fetchABI({ contractAddress });
-  if (abi) {
-    await writeJSON(`./data/abi/${contractAddress}.json`, abi);
+  const pools = ethereumMainPools.data.poolData;
+  for (const pool of pools) {
+    await sleep(500);
+    const abi = await explorers.etherscan.mainnet.fetchABI({ contractAddress: pool.address });
+    if (abi) {
+      await writeJSON(`./data/abi/ethereum.${pool.address}.json`, abi);
+    }
   }
 };
 
