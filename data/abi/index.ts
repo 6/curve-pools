035d3fe4c162b5dfea245a0839a6c00f6a4c4160
Peter Graham <peterghm@gmail.com>
2022-07-28 09:06:06 -0400

    do one level higher grouping

--- data/abi/index.ts
+++ data/abi/index.ts
@@ -1,28 +1,9 @@
-// todo: is there a better way?
-import ethereumMain from './ethereum.main.json';
-import ethereumCrypto from './ethereum.crypto.json';
-import ethereumFactory from './ethereum.factory.json';
-import arbitrumMain from './arbitrum.main.json';
-import arbitrumCrypto from './arbitrum.crypto.json';
-import arbitrumFactory from './arbitrum.factory.json';
-import optimismMain from './optimism.main.json';
-import optimismCrypto from './optimism.crypto.json';
-import optimismFactory from './optimism.factory.json';
+import ethereum from './ethereum.json';
+import arbitrum from './arbitrum.json';
+import optimism from './optimism.json';
 
 export const abis = {
-  ethereum: {
-    main: ethereumMain,
-    crypto: ethereumCrypto,
-    factory: ethereumFactory,
-  },
-  arbitrum: {
-    main: arbitrumMain,
-    crypto: arbitrumCrypto,
-    factory: arbitrumFactory,
-  },
-  optimism: {
-    main: optimismMain,
-    crypto: optimismCrypto,
-    factory: optimismFactory,
-  },
+  ethereum,
+  arbitrum,
+  optimism,
 };
--- tasks/update-pool-abis.ts
+++ tasks/update-pool-abis.ts
@@ -2,25 +2,33 @@ import { sleep } from '../src/utils/sleep';
 import { explorers } from '../src/utils/etherscan';
 import { writeJSON } from '../src/utils/write-json';
 import { pools } from '../data/pools';
-import { CURVE_NETWORKS, CURVE_POOL_TYPES } from '../src/utils/curve.constants';
+import { CURVE_NETWORKS, CURVE_POOL_TYPES, PoolType } from '../src/utils/curve.constants';
+
+type ABIsForNetwork = {
+  [poolType in PoolType]: {
+    [contractAddress: string]: Record<string, unknown>;
+  };
+};
 
 const main = async () => {
   for (const network of CURVE_NETWORKS) {
+    const abis: ABIsForNetwork = { main: {}, crypto: {}, factory: {} };
     for (const poolType of CURVE_POOL_TYPES) {
-      const abis: { [contractAddress: string]: Record<string, unknown> } = {};
       const poolData = pools[network][poolType].poolData;
       for (const pool of poolData) {
         await sleep(500);
         const contractAddress = pool.address;
         const abi = await explorers[network].mainnet.fetchABI({ contractAddress });
         if (abi) {
-          abis[contractAddress] = abi;
+          abis[poolType][contractAddress] = abi;
         } else {
           console.warn(`Unable to fetch ABI for ${contractAddress}`);
+          // TODO: ensure here that the error is infact "unverified contract".
+          // otherwise should probably fail with different error
         }
       }
-      await writeJSON(`./data/abi/${network}.${poolType}.json`, abis);
     }
+    await writeJSON(`./data/abi/${network}.json`, abis);
   }
 };
 
