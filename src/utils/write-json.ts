a83c9cdb6916f12bf6025015178f3671bfb052ca
Peter Graham <peterghm@gmail.com>
2022-07-28 08:47:53 -0400

    Update write-json.ts

--- src/utils/write-json.ts
+++ src/utils/write-json.ts
@@ -2,6 +2,7 @@ import { PathLike } from 'fs';
 import { FileHandle, writeFile } from 'fs/promises';
 import prettier from 'prettier';
 
+// Write JSON to a file with `prettier` formatting:
 export const writeJSON = (file: PathLike | FileHandle, data: unknown): Promise<void> => {
   const prettyJSON = prettier.format(JSON.stringify(data), {
     parser: 'json',
