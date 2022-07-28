d4e69b078a3724191ee4f38bc790daed5659e69b
6 <peterghm@gmail.com>
2022-07-28 19:01:21 -0400

    Include ABI interface instance alongside Pool metadata (#9)
    * include interface alongside pool
    
    * remove need to specify poolInterface when parsing transaction

--- data/pools/index.ts
+++ data/pools/index.ts
@@ -1,7 +1,14 @@
 import path from 'path';
 import { readFile } from 'fs/promises';
+import { ethers } from 'ethers';
+import lodash from 'lodash';
 import { Network, PoolType } from '../../src/utils/curve.constants';
 import { CurvePoolMetadata, FetchPoolsResponse } from '../../src/utils/curve-api';
+import { getABIs } from '../abis';
+
+export interface CurvePoolWithInterface extends CurvePoolMetadata {
+  interface: ethers.utils.Interface;
+}
 
 interface GetPoolsProps {
   network: Network;
@@ -10,13 +17,27 @@ interface GetPoolsProps {
 export const getPools = async ({
   network,
   poolType,
-}: GetPoolsProps): Promise<Array<CurvePoolMetadata>> => {
+}: GetPoolsProps): Promise<Array<CurvePoolWithInterface>> => {
   const poolsFile = await readFile(
     path.resolve(__dirname, `./${network}.${poolType}.json`),
     'utf8',
   );
   const poolsResponse = JSON.parse(poolsFile) as FetchPoolsResponse;
-  return poolsResponse.data.poolData;
+  const pools = poolsResponse.data.poolData;
+  const abis = await getABIs({ network, poolType });
+  return lodash.compact(
+    pools.map((pool) => {
+      const abi = abis[pool.address.toLowerCase()];
+      if (!abi) {
+        // Ignore pools that don't have a verified contract yet:
+        return;
+      }
+      return {
+        ...pool,
+        interface: new ethers.utils.Interface(JSON.stringify(abi)),
+      };
+    }),
+  );
 };
 
 interface GetPoolProps {
@@ -28,7 +49,7 @@ export const getPool = async ({
   network,
   poolType,
   contractAddress,
-}: GetPoolProps): Promise<CurvePoolMetadata> => {
+}: GetPoolProps): Promise<CurvePoolWithInterface> => {
   const lowercaseContractAddress = contractAddress.toLowerCase();
   const pools = await getPools({ network, poolType });
   const pool = pools.find((p) => p.address.toLowerCase() === lowercaseContractAddress);
