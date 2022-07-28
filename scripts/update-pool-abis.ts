199cc2eaaf8ed65a8d0fedc765a0534fdfb070ac
Peter Graham <peterghm@gmail.com>
2022-07-28 00:08:20 -0400

    import all abis and txs

--- scripts/update-pool-abis.ts
+++ scripts/update-pool-abis.ts
@@ -1,11 +1,10 @@
 import { sleep } from '../src/utils/sleep';
 import { explorers } from '../src/utils/etherscan';
 import { writeJSON } from '../src/utils/write-json';
-import ethereumMainPools from '../data/pools/ethereum.main.json';
+import { pools } from '../data/pools';
 
 const main = async () => {
-  const pools = ethereumMainPools.data.poolData;
-  for (const pool of pools) {
+  for (const pool of pools.ethereum.main.poolData) {
     await sleep(500);
     const abi = await explorers.etherscan.mainnet.fetchABI({ contractAddress: pool.address });
     if (abi) {
--- scripts/update-pool-transactions.ts
+++ scripts/update-pool-transactions.ts
@@ -1,14 +1,15 @@
 import { sleep } from '../src/utils/sleep';
 import { explorers } from '../src/utils/etherscan';
 import { writeJSON } from '../src/utils/write-json';
+import { pools } from '../data/pools';
 
 const main = async () => {
-  const contractAddress = '0xbEbc44782C7dB0a1A60Cb6fe97d0b483032FF1C7';
-
-  await sleep(500);
-  const txlist = await explorers.etherscan.mainnet.fetchTxList({ contractAddress });
-  if (txlist) {
-    await writeJSON(`./data/tx/${contractAddress}.json`, txlist);
+  for (const pool of pools.ethereum.main.poolData) {
+    await sleep(500);
+    const txlist = await explorers.etherscan.mainnet.fetchTxList({ contractAddress: pool.address });
+    if (txlist) {
+      await writeJSON(`./data/tx/ethereum.${pool.address}.json`, txlist);
+    }
   }
 };
 
