73807006ecdcf697301df23f7a4191dd0533010f
Peter Graham <peterghm@gmail.com>
2022-07-28 22:17:39 -0400

    finish script to process transactions

--- tasks/process-pool-transactions.ts
+++ tasks/process-pool-transactions.ts
@@ -1,7 +1,9 @@
-import { writeJSON } from '../src/utils/write-json';
-import { ethers } from 'ethers';
+import lodash from 'lodash';
 import { getTxs } from '../data/txs';
 import { getPools } from '../data/pools';
+import { writeJSON } from '../src/utils/write-json';
+import { CurveAssetTypeName } from '../src/utils/curve.constants';
+import { parseTransaction } from '../src/utils/parse-transaction';
 
 const main = async () => {
   // For now, just focus on limited set of data:
@@ -10,12 +12,28 @@ const main = async () => {
 
   const pools = await getPools({ network, poolType });
   for (const pool of pools) {
+    // Skip pools with uncorrelated/other assets like tricrypto
+    if (
+      pool.assetTypeName === CurveAssetTypeName.UNKNOWN ||
+      pool.assetTypeName === CurveAssetTypeName.OTHER
+    ) {
+      console.info(
+        `[process-pool-transactions] skipping unknown/other pool ${pool.name} (${network} => ${pool.address})`,
+      );
+      continue;
+    }
+
     const contractAddress = pool.address;
     const txs = await getTxs({ network, poolType, contractAddress });
 
-    txs.map((tx) => {
-      const decodedInput = pool.interface.parseTransaction({ data: tx.input, value: tx.value });
-    });
+    const parsedTxs = lodash.compact(
+      txs.map((tx) => {
+        const { transaction } = parseTransaction({ pool, tx });
+        return transaction;
+      }),
+    );
+
+    writeJSON(`./src/processed-data/txs/${network}.${poolType}.json`, parsedTxs);
 
     break;
   }
