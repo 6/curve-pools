0cb648d91a4bf47395a577c2f8f53985bfd72122
Peter Graham <peterghm@gmail.com>
2022-07-29 15:20:23 -0400

    TVL threshold

--- tasks/process-pool-metadata.ts
+++ tasks/process-pool-metadata.ts
@@ -1,9 +1,11 @@
 import lodash from 'lodash';
-import { getTxs } from '../data/txs';
+import { Decimal } from 'decimal.js';
 import { CurvePoolExtended, getPools } from '../data/pools';
 import { writeJSON } from '../src/utils/write-json';
 import { CURVE_NETWORKS, CURVE_POOL_TYPES } from '../src/utils/curve.constants';
-import { CurveTransaction, parseTransaction } from '../src/utils/parse-transaction';
+
+// only analyze pools with sufficient tvl
+const minimumTVL = new Decimal(10000000);
 
 const main = async () => {
   let allPools: Array<CurvePoolExtended> = [];
@@ -16,8 +18,12 @@ const main = async () => {
 
   allPools = lodash.orderBy(allPools, 'usdTotal', 'desc');
   const correlatedPools = allPools.filter((pool) => pool.poolType !== 'crypto');
+  const poolsWithSufficientTVL = correlatedPools.filter((pool) =>
+    new Decimal(pool.usdTotal).gte(minimumTVL),
+  );
+  console.log(`Found ${poolsWithSufficientTVL.length} pools with gte $${minimumTVL}`);
 
-  for (const pool of correlatedPools.slice(0, 20)) {
+  for (const pool of poolsWithSufficientTVL) {
     console.log(pool.network, pool.poolType);
     console.log(pool.shortName, pool.name, pool.usdTotal, pool.address);
   }
