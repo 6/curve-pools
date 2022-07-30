b53403d6537f8f058ff019df9ecaa10e8d6463c0
Peter Graham <peterghm@gmail.com>
2022-07-30 17:46:33 -0400

    sample theme extension

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { DashboardScreen } from './screens/dashboard/dashboard.screen';
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      900: '#1a365d',
+      800: '#153e75',
+      700: '#2a69ac',
+    },
+  },
+});
 
 export const App = () => {
   return (
-    <ChakraProvider resetCSS>
+    <ChakraProvider resetCSS theme={theme}>
       <DashboardScreen />
     </ChakraProvider>
   );
