4a0b228dc2cfc06d8026430a78e5b528beed75ec
Peter Graham <peterghm@gmail.com>
2022-07-30 18:26:37 -0400

    text to describe dashboard

--- src/screens/dashboard/dashboard.screen.tsx
+++ src/screens/dashboard/dashboard.screen.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import { Container, Heading, Text, Center, Wrap } from '@chakra-ui/react';
 import { useProminentTransactions } from '../../hooks/use-prominent-transactions';
 import { useTopPools } from '../../hooks/use-top-pools';
 
@@ -10,5 +12,16 @@ export const DashboardScreen = () => {
 
   console.log('prominent txs:', prominentTxs);
 
-  return <h1>Dashboard</h1>;
+  return (
+    <Container maxW="800px" paddingTop="10">
+      <Heading fontSize="3xl" textAlign="center">
+        Curve Pools Scanner
+      </Heading>
+      <Center>
+        <Text marginTop="5" fontSize="xl" textAlign="center" maxW="md" alignSelf="center">
+          Explore the top Curve.fi pools, identify imbalances, and find large changes in liquidity.
+        </Text>
+      </Center>
+    </Container>
+  );
 };
